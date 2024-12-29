befa9aae0798aae922af937144de5d5d44041c0f
Frompaje <yanedwards11@gmail.com>
2024-12-29 16:42:45 -0300

    feat:cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { logger } from "./config/logger";
 import { env } from "./infra/env/env";
 import { router } from "./infra/routes";
 import express from "express";
+import cors from "cors"
+
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors())
 
 app.listen(env.PORT, () => {
    logger.info('Server is running')
 });
+
+
