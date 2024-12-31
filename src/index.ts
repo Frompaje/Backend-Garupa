dddec01a0895b061b009b3015ee14ba32041b84d
Frompaje <yanedwards11@gmail.com>
2024-12-31 16:15:39 -0300

    refact: change positon cors

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use(router);
 app.use(cors())
+app.use(router);
 
 app.listen(env.PORT, () => {
    logger.info('Server is running')
