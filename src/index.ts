1cc0874f1a10ded0226977997501d484440bd74c
Frompaje <yanedwards11@gmail.com>
2024-12-31 17:06:00 -0300

    refact: config cors

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,16 @@ import { logger } from "./config/logger";
 import { env } from "./infra/env/env";
 import { router } from "./infra/routes";
 import express from "express";
-import cors from "cors"
-
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-   origin: "http://localhost:5173/",
-   methods: ["GET", "POST"],
-   allowedHeaders: ["Content-Type", "Authorization"]
-}))
+
+app.options("*", cors());
+
 app.use(router);
 
 app.listen(env.PORT, () => {
-   logger.info('Server is running')
+  logger.info("Server is running");
 });
-
-
