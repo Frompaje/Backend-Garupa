27198a33f51184ce2aa82d49547e512f13758112
Frompaje <yanedwards11@gmail.com>
2025-01-01 17:36:02 -0300

    feat: list transfer repository

--- src/app/repository/index.ts
+++ src/app/repository/index.ts
@@ -2,7 +2,13 @@ import { pg } from "../../infra/database";
 import { TransferInput } from "./types";
 
 export class TransferRepository {
-  async create({ id, external_id, amount, expected_on, status }: TransferInput) {
+  async create({
+    id,
+    external_id,
+    amount,
+    expected_on,
+    status,
+  }: TransferInput) {
     await pg.query(
       "INSERT INTO TRANSFERS(id,external_id,amount,expected_on,status) VALUES ($1,$2,$3,$4,$5)",
       [id, external_id, amount, expected_on, status]
@@ -22,6 +28,14 @@ export class TransferRepository {
     const { rows } = await pg.query(
       "SELECT id,external_id,amount,expected_on,status,created_at,updated_at FROM TRANSFERS WHERE external_id = $1",
       [external_id]
+    );
+
+    return rows[0];
+  }
+
+  async listAllTransfers() {
+    const { rows } = await pg.query(
+      "SELECT id,external_id,amount,expected_on,status,created_at,updated_at FROM TRANSFERS"
     );
 
     return rows[0];
